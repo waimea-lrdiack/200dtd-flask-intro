04218994e71e880cdbf299baf9f9a9ed63d64c0e
waimea-lrdiack <lrdiack@waimea.school.nz>
2025-05-22 12:24:00 +1200

    created a form

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from flask import redirect
 from random import randint
 
 # create the app
@@ -18,3 +19,18 @@ def about():
 def random():
     randNum = (randint(1, 1000))
     return render_template('pages/random.jinja', number=randNum)
+
+@app.get("/number/<int:num>")
+def analyseNumber(num):
+    print(f"You entered: {num}")
+    return render_template('pages/number.jinja', number=num)
+
+
+@app.errorhandler(404)
+def notFound(e):
+    return render_template("pages/404.jinja")
+
+
+@app.get("/form/")
+def form():
+    return render_template("pages/form.jinja")
